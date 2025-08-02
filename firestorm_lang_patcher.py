5fbf10a66f63a3b78b9eb562e16172f086a9d36c
Yukinoroh <Yukinoroh@users.noreply.github.com>
2025-08-02 13:02:41 -0400

    Update firestorm_lang_patcher.py

--- firestorm_lang_patcher.py
+++ firestorm_lang_patcher.py
@@ -9,14 +9,14 @@ lang=os.getenv('LANG')
 before_underscore_pos=lang.find('_')
 lang=lang[0:before_underscore_pos:]
 
-# Gets default Firestorm installation
-installedpath=""
+# Gets default Firestorm installation path
+path=""
 if osname == "Windows":
-	installedpath="C:\Program Files\Firestorm-Releasex64"
+	path="C:\Program Files\Firestorm-Releasex64"
 elif osname == "Linux":
-	installedpath="/home/"+os.environ.get('USER', os.environ.get('USERNAME'))+"/"
+	path="/home/"+os.environ.get('USER', os.environ.get('USERNAME'))+"/"
 
-# Prompts user for a Folder
+# Prompts user for a foler
 message="Choose Firestorm main folder"
 if lang == "ca":
 	message="Tria la carpeta principal de Firestorm"
@@ -24,7 +24,11 @@ elif lang == "fr":
 	message="Choisissez le dossier principal de Firestorm"
 elif lang == "uk":
 	message="Виберіть головну папку Firestorm"
-folder=plyer.filechooser.choose_dir(path=installedpath,title=message)
+path=plyer.filechooser.choose_dir(path=path,title=message)
 
-# Make sure this is a Firestorm folder
+# Patches language settigns
 #TODO
+
+# Installs translations
+#TODO
+
